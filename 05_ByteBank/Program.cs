1ded12e892c10c451055486ef33316ebf0e11184
iagoa <iagoantunes.f@gmail.com>
2021-11-17 14:02:58 -0300

    Iago Antunes Ferreira

--- 05_ByteBank/Program.cs
+++ 05_ByteBank/Program.cs
@@ -10,12 +10,46 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
+            //Cliente
+            //Cliente gabriela = new Cliente();
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "434.562.878-20";
 
-            gabriela.nome = "Gabriela";
-            gabriela.profissao = "Desenvolvedora C#";
-            gabriela.cpf = "434.562.878-20";
+            //Conta
+            ContaCorrente conta = new ContaCorrente();
+            //conta.titular = gabriela;
+            //conta.titular = new Cliente();
+            //conta.titular.nome = "Gabriela Costa";
+            //conta.titular.cpf = "434.562.878-20";
+            //conta.titular.profissao = "Desenvolvedora C#";
 
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            if(conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referência em conta.titular é null");
+            }
+
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
+
+            Console.ReadLine();
         }
     }
 }
+/*
+ * Realizar o relacionamento dentre classes atraves de composição
+ * Vantagens de se isolar informações repetidas e motra classe
+ * como namespsace funcionam no c#
+ * o que forma o nome completo de uma classe
+ * como as diretivas using sao usadas 
+ * o valor null*/
