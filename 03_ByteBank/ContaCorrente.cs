a14dcd1dc428cc4a848bc703c97c94d2fb54993c
iagoa <iagoantunes.f@gmail.com>
2021-11-17 10:41:13 -0300

    Iago Antunes Ferreira

---
+++ 03_ByteBank/ContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+
+
+
+}
---
+++ 03_ByteBank/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();//Referencia
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();//Referencia
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referencia: "+(contaDaGabriela == contaDaGabrielaCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+            Console.WriteLine("Igualdade de tipo de valor: "+(idade == idadeMaisUmaVez));
+
+            contaDaGabriela = contaDaGabrielaCosta;//Apontam para o mesmo endereco
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
+            if(contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+
+
+            Console.ReadLine();
+            /*Como acessar e alterar campos de um objeto
+             * valores padores de campos de classes
+             * tipos de referencia e tipos de valor
+             * inicialização de campos
+             * como funciona a atribuição de uma referencia a uma variavel
+             */ 
+        }
+    }
+}
