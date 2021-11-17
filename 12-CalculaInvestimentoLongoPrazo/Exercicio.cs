3e29bdef4a61daf7149df7818e811c7891dd23d6
iagoa <iagoantunes.f@gmail.com>
2021-11-17 08:23:59 -0300

    Iago Antunes Ferreira

---
+++ 12-CalculaInvestimentoLongoPrazo/Exercicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_CalculaInvestimentoLongoPrazo
+{
+    internal class Exercicio
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+
+            for(contador = 1;contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+
+            /*while(contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }*/
+        }
+    }
+}
