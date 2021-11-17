eb12d669472b4b152ef99920ea96cf6e9572f20b
iagoa <iagoantunes.f@gmail.com>
2021-11-17 13:34:22 -0300

    Iago Antunes Ferreira

---
+++ 05_ByteBank/ContaCorrente.cs
@@ -0,0 +1,52 @@
+﻿
+namespace _05_ByteBank
+{
+    class ContaCorrente
+    {
+        public Cliente titular;
+
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+
+    }
+}
+/*
+ * como definir metodos com parametros e retornos
+ * como retornar algo usando a palavra chave return antes da expressao do resultado;
+ * o uso do return para retornar antecipadamente a execução de um metodo
+ * como usar a referencia this para acessar um atributo
+ * qque podemos passar uma referencia como parametro do metodo
+ * metodos soa chamadas a partir da referencia usando o operador
+ */
