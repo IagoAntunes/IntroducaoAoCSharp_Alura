e2ce6a635c4089f4f2d2121666a3f6a994312529
iagoa <iagoantunes.f@gmail.com>
2021-11-19 09:30:45 -0300

    Iago Antunes Ferreira

---
+++ ByteBankExcecoes/ContaCorrente.cs
@@ -0,0 +1,80 @@
+﻿
+namespace ByteBankExcecoes
+{
+    class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+        public Cliente titular { get; set; }
+        public static int TotalDeContasCriadas { get;private set; }//Faz parte da CLASSE
+        
+        public int agencia { get; set; }
+        public int numero { get; set; }
+        public double saldo = 100;
+
+        public double Saldo
+        {
+            get 
+            { 
+                return saldo; 
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)//Construtor
+        {
+            this.agencia = agencia;
+            this.numero = numero;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+
+            TotalDeContasCriadas++;
+        }
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+
+    }
+}
+/*
+ * como definir metodos com parametros e retornos
+ * como retornar algo usando a palavra chave return antes da expressao do resultado;
+ * o uso do return para retornar antecipadamente a execução de um metodo
+ * como usar a referencia this para acessar um atributo
+ * qque podemos passar uma referencia como parametro do metodo
+ * metodos soa chamadas a partir da referencia usando o operador
+ */
---
+++ ByteBankExcecoes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }catch (NullReferenceException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+            Console.ReadLine();
+        }
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisao de 10 por " + divisor + " =" + resultado);
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Nao e possivel fazer divisao por 0");
+            }
+
+        }
+        private static int Dividir(int numero,int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.saldo);
+            return numero / divisor;
+        }
+    }
+}
+/*01
+ * Como funciona a pilha de chamadas (CallStack) no .NET
+ * Propagar erros com retorno de metodos nao é correto
+ * Pordemos usar as exceções do .NET
+ * Para tratar exceções do .NET , basta usar os blocos try/catch
+ * As exceções sao representadas por objetos e duas propriedades importantes sao
+ * a Message e StackTrace*/
