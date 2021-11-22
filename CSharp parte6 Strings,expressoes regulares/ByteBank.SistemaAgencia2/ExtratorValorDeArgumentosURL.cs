17a709c9c8f728c9d5bc145e2e3d3e37579193c8
iagoa <iagoantunes.f@gmail.com>
2021-11-22 15:38:01 -0300

    Iago Antunes Ferreira

---
+++ CSharp parte6 Strings,expressoes regulares/ByteBank.SistemaAgencia2/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia2
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argument URL nao pode ser nula ou vazia",nameof(url));
+            }
+    
+            URL = url;
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+            
+
+        }
+        //MoedaOrigem=real&moedaDestino=dolar
+        public string getValor(string nomeParametro)
+        {
+            return "";
+        }
+    }
+}
--- CSharp parte6 Strings,expressoes regulares/ByteBank.SistemaAgencia2/Program.cs
+++ CSharp parte6 Strings,expressoes regulares/ByteBank.SistemaAgencia2/Program.cs
@@ -10,23 +10,57 @@ namespace ByteBank.SistemaAgencia2
     {
         static void Main(string[] args)
         {
-            string url = "paginas?argumentos";
+
+
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("TamanhoString" + nomeArgumento.Length);
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+            Console.ReadLine();
+
+
+            //
+            string textoVazio = "";
+            string textoNull = null;
+
+            Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(string.IsNullOrEmpty(textoNull));
+            Console.ReadLine();
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
             int indiceInterrogacao = url.IndexOf('?');
 
             Console.WriteLine(indiceInterrogacao);
 
             Console.WriteLine(url);
 
-            string argumentos = url.Substring(indiceInterrogacao);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
 
             Console.WriteLine(argumentos);
+
 
 
             Console.ReadLine();
         }
     }
 }
-/*
+/*01
  * No c# e .NET strings sao imutaveis
  * Podemos criar uma nova string a partir da porção de outra , com o metodo Substring;
  */
+
+/*02
+ * Metodo IndexOF
+ * Metodo estatico IsNullOrEmpty
+ * A palavra reservada string é , na verdade o tipo String
+ * A palavra reservada int é , na verdade o tipo int32
+ * A palavra reservada double é na verdade o tipo Double
+ * A sobrecarga String::IndexOf(string)
+ * A propriedade String::Length*/
