9a7f6b862dde90c3455f10eb14d3dc28588acc30
iagoa <iagoantunes.f@gmail.com>
2021-11-23 15:48:21 -0300

    Iago Antunes Ferreira

---
+++ CSharp parte7 Array e tipos genericos/SistemaAgencia03/ListaDeContaCorrente.cs
@@ -0,0 +1,52 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAgencia03
+{
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+        public ListaDeContaCorrente(int capacidadeInicial=5)
+        {
+            _itens = new ContaCorrente[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificaCapacidade(_proximaPosicao + 1);
+
+            Console.WriteLine($"Adicionando item na posicao {_proximaPosicao}");
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+        private void VerificaCapacidade(int tamanhoNecessario)
+        {
+            if(_itens.Length >= tamanhoNecessario) { 
+                return ;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+            if(novoTamanho< tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            Console.WriteLine("Aumentando capacidade da lista");
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            for(int indice = 0;indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+            }
+            _itens = novoArray;
+
+
+        }
+    }
+}
--- CSharp parte7 Array e tipos genericos/SistemaAgencia03/Program.cs
+++ CSharp parte7 Array e tipos genericos/SistemaAgencia03/Program.cs
@@ -11,23 +11,40 @@ namespace SistemaAgencia03
     {
         static void Main(string[] args)
         {
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 5679754));
+            lista.Adicionar(new ContaCorrente(874, 5679865));
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 5679754));
+            lista.Adicionar(new ContaCorrente(874, 5679865));
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 5679754));
+            lista.Adicionar(new ContaCorrente(874, 5679865));
+
+
+
+            Console.ReadLine();
+        }
+        static void TestaArrayDeContaCorrrente()
+        {
             ContaCorrente[] contas = new ContaCorrente[]
-            {
+{
                 new ContaCorrente(876, 345777),
                 new ContaCorrente(123, 123123),
                 new ContaCorrente(456, 123987)
-            };
+};
             //contas[0] = new ContaCorrente(876, 345777);
             //contas[1] = new ContaCorrente(123, 123123);
             //contas[2] = new ContaCorrente(456, 123987);
 
-            for(int indice = 0;indice < contas.Length; indice++)
+            for (int indice = 0; indice < contas.Length; indice++)
             {
                 Console.WriteLine($"Conta {indice} {contas[indice].Numero}");
             }
-
-            Console.ReadLine();
         }
+
         static void TestaArrayInt()
         {
             int[] idades = new int[6];
@@ -60,9 +77,13 @@ namespace SistemaAgencia03
         }
     }
 }
-/*
+/*01
  * Sintaxe de criação de variaveis de array
  * O valor padrao é utilizado em todas as posições de um array ao ser criado
  * A propriedade Lenght de arrays
  * A sintaxe de inicialização de array new int[]{5,12,64};8/
  */
+/*02
+ * Mais logica com arrays
+ * Argumentos opcionais
+ * Argumentos nomeados*/
