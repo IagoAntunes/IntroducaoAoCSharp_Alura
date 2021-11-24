0138da9602e2ffb4447686a9921e552b31fc0048
iagoa <iagoantunes.f@gmail.com>
2021-11-24 15:30:00 -0300

    Iago Antunes Ferreira

--- SistemaAgencia04/Program.cs
+++ SistemaAgencia04/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Modelos;
 using SistemaAgencia04.Extensoes;
 
 namespace SistemaAgencia04
@@ -11,23 +12,59 @@ namespace SistemaAgencia04
     {
         static void Main(string[] args)
         {
-            List<int> idades = new List<int>();
+            var contas = new List<ContaCorrente>()
+            {
+                new ContaCorrente(123,456789),
+                new ContaCorrente(345,456789),
+                new ContaCorrente(164,456789),
+                new ContaCorrente(986,456789)
+            };
+            contas.Sort();
+
+            foreach (var conta in contas)
+            {
+                Console.WriteLine($"Conta numero {conta.Numero},ag:{conta.Agencia}");
+            }
+
+
+    
+
+            Console.ReadLine();
+        }
+        static void TestaSorte()
+        {
+            var idades = new List<int>();
+            var nomes = new List<string>()
+            {
+                "Joao,"
+                + "Luana,"
+                + "Wellington,"
+                + "Ana"
+            };
+
 
             idades.Add(5);
             idades.Add(23);
 
-
             idades.AdicionarVarios(23, 40, 52, 12);
-            
+            idades.AdicionarVarios(99, -1);
+
+            idades.Sort();
+            nomes.Sort();
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
 
             for (int i = 0; i < idades.Count; i++)
             {
                 Console.WriteLine($"Idade Atual:{idades[i]}");
             }
-
-            Console.ReadLine();
         }
     }
+    
 }
 /*public static void EscreverNaTela(this object obj){
  * Console.WriteLine(obj)
@@ -44,3 +81,7 @@ namespace SistemaAgencia04
  * como criar metodos de extensao genericos
  * como o compilar encontra os metodos de xtensao
  */
+/*03
+ * A interferencia de variaveis locais com o var
+ * Sintaxe de inicialização de listas
+ * O metodo Sort*/
